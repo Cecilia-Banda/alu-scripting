a0d7044b10f613f5183dfb5dd0ee2103d7c404fe
Cecilia_Banda <105134294+Cecilia-Banda@users.noreply.github.com>
2022-10-21 20:19:16 +0200

    Update 0-subs.py

--- api.advanced/0-subs.py
+++ api.advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Contains the number_of_subscribers function"""
+""""Subs function"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """"Contains the number_of_subscribers function"""
+    """"Subs fuction"""
     url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
 
